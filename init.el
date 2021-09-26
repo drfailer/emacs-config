3cfc3c3c36db3f17ee927a90d496e4356d3742fd
drfailer <drfailer.42@gmail.com>
2021-09-26 17:57:05 +0200

    fix use-package + more bindings

--- init.el
+++ init.el
@@ -1,19 +1,28 @@
 ;; Melpa
 (require 'package)
-(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
+(package-refresh-contents)
 (package-initialize)
 
-;; Bootstrap `use-package'
+;; usepackage
 (unless (package-installed-p 'use-package)
-(package-refresh-contents)
-(package-install 'use-package))
-
-(use-package which-key
-:ensure t
-:config
-(which-key-mode))
+  (package-install 'use-package))
+(setq use-package-always-ensure t)
 
 ;; Load configuration
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(peep-dired all-the-icons-dired org-bullets auto-complete key-chord general evil-tutor evil-collection evil which-key doom-modeline all-the-icons gruvbox-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'dired-find-alternate-file 'disabled nil)
