b14a9c9f77e9d86fd5d5d4cd5c548eeef4ac4e93
drfailer <drfailer.42@gmail.com>
2021-11-06 10:23:43 +0100

    better config

--- init.el
+++ init.el
@@ -1,16 +1,19 @@
-;; Melpa
 (require 'package)
+
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-refresh-contents)
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+ 	     '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
-;; usepackage
+;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
-(setq use-package-always-ensure t)
 
-;; Load configuration
+
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -18,11 +21,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(peep-dired all-the-icons-dired org-bullets auto-complete key-chord general evil-tutor evil-collection evil which-key doom-modeline all-the-icons gruvbox-theme use-package)))
+   '(dashboard treemacs hungry-delete yasnippet-snippets yasnippet which-key use-package peep-dired org-bullets key-chord gruvbox-theme general evil-tutor evil-collection doom-modeline dired-open auto-complete all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)
