7cab9870dd7a27b443cbb5fef361a987a2a1a590
drfailer <drfailer.42@gmail.com>
2021-11-21 11:44:21 +0100

    ivy swiper and more + more bindings

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard treemacs hungry-delete yasnippet-snippets yasnippet which-key use-package peep-dired org-bullets key-chord gruvbox-theme general evil-tutor evil-collection doom-modeline dired-open auto-complete all-the-icons-dired)))
+   '(ivy-rich counsel yasnippet-snippets which-key use-package treemacs peep-dired org-bullets key-chord hungry-delete gruvbox-theme general evil-tutor evil-collection doom-modeline dired-open dashboard auto-complete all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
